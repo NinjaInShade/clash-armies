e864f9d0ae0f939349a977d4487ad49f22e60e9b
Leon Michalak <leonmichalak6@gmail.com>
2024-04-20 13:03:52 +0100

    Add BASE_APP_URL env option that will correctly set the google oAuth callback

--- src/lib/server/auth/lucia.ts
+++ src/lib/server/auth/lucia.ts
@@ -5,11 +5,16 @@ import { sqlAdapter } from "./adapter";
 import { dev } from "$app/environment";
 import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET } from '$env/static/private';
 
+const BASE_APP_URL = dev ? 'http://localhost:5173' : process.env.BASE_APP_URL;
+
 if (typeof GOOGLE_AUTH_CLIENT_ID !== 'string') {
     throw new Error('Expected google auth client id to be defined');
 }
 if (typeof GOOGLE_AUTH_SECRET !== 'string') {
     throw new Error('Expected google auth secret to be defined');
+}
+if (!dev && typeof BASE_APP_URL !== 'string') {
+	throw new Error('Expected base app url to be defined in production');
 }
 
 const adapter = new sqlAdapter(db);
@@ -29,7 +34,7 @@ export const lucia = new Lucia(adapter, {
 	}
 });
 
-export const google = new Google(GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET, 'http://localhost:5173/login/google/callback');
+export const google = new Google(GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET, `${BASE_APP_URL}/login/google/callback`);
 
 declare module "lucia" {
 	interface Register {
