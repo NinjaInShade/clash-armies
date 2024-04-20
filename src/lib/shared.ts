7452a51db3f346584395e132ce579818a36e427a
Leon Michalak <leonmichalak6@gmail.com>
2024-04-20 13:03:51 +0100

    Add banners array, remove ValidationError

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,13 +1,17 @@
-export class ValidationError extends Error {
-	public name = 'ValidationError';
-	public field?: string;
-
-	constructor(validation?: { field: string; message?: string }) {
-		if (validation) {
-			super(validation.message);
-			this.field = validation.field;
-		} else {
-			super();
-		}
-	}
-}
+// should match banner file names (without extension) in /static/clash/banners/*
+export const BANNERS = [
+	'clan-capital-2',
+	'clashiversary',
+	'th-16',
+	'books-of-clash',
+	'chess',
+	'clan-capital',
+	'dark-ages-2',
+	'dark-ages',
+	'halloween',
+	'lunar-new-year',
+	'monument',
+	'space',
+	'summer',
+	'valentines'
+] as const;
