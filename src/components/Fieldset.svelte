5496404a583f0c61c457e31a4b3012e33b66bf40
Leon Michalak <leonmichalak6@gmail.com>
2024-04-20 13:03:24 +0100

    Add Fieldset component

---
+++ src/components/Fieldset.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { setContext, type Snippet } from 'svelte';
+
+	type Props = {
+		/** Sets the label */
+		label: string;
+		/** Sets the label's "for" attribute */
+		htmlName: string;
+		/** Input to render inside fieldset */
+		children: Snippet;
+	};
+	let { label, htmlName, children } = $props<Props>();
+
+	// Sets the context for children form elements to use
+	setContext('htmlName', htmlName);
+</script>
+
+<fieldset>
+	<label for={htmlName}>{label}</label>
+	{@render children()}
+</fieldset>
+
+<style>
+	fieldset {
+		border: none;
+		display: flex;
+		position: relative;
+		flex-direction: column;
+		align-items: flex-start;
+		user-select: none;
+		white-space: nowrap;
+		gap: 4px;
+	}
+
+	label {
+		color: var(--grey-300);
+		font-size: var(--fs);
+		line-height: var(--fs-lh);
+		font-weight: 500;
+	}
+</style>
