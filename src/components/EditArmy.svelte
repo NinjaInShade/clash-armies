f0a3d949251feedda3bb74df2415ea763f2d3a19
Leon Michalak <leonmichalak6@gmail.com>
2024-04-20 13:03:50 +0100

    Utilise svelte snippets for rendering filler cards + don't make them buttons w/ add icons
    The vision I had in my head of what I could do with the filler cards being buttons isn't actually that realistic...

--- src/components/EditArmy.svelte
+++ src/components/EditArmy.svelte
@@ -229,16 +229,7 @@
 					</button>
 				</li>
 			{/each}
-			{#each Array.from({ length: ARMY_CREATE_TROOP_FILLER - troopUnits.length > 0 ? ARMY_CREATE_TROOP_FILLER - troopUnits.length : 0 }) as filler}
-				<button class="object-card">
-					<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" class="add-icon">
-						<path
-							d="M15 0C11.0367 0.0480892 7.24926 1.64388 4.44657 4.44657C1.64388 7.24926 0.0480892 11.0367 0 15C0.0480892 18.9633 1.64388 22.7507 4.44657 25.5534C7.24926 28.3561 11.0367 29.9519 15 30C18.9633 29.9519 22.7507 28.3561 25.5534 25.5534C28.3561 22.7507 29.9519 18.9633 30 15C29.9519 11.0367 28.3561 7.24926 25.5534 4.44657C22.7507 1.64388 18.9633 0.0480892 15 0ZM23.5714 16.0714H16.0714V23.5714H13.9286V16.0714H6.42857V13.9286H13.9286V6.42857H16.0714V13.9286H23.5714V16.0714Z"
-							fill="#5C5C5C"
-						/>
-					</svg>
-				</button>
-			{/each}
+			{@render filler(ARMY_CREATE_TROOP_FILLER - troopUnits.length > 0 ? ARMY_CREATE_TROOP_FILLER - troopUnits.length : 0)}
 		</ul>
 		<h3>Select troops</h3>
 		<ul class="picker-grid">
@@ -318,16 +309,7 @@
 					</button>
 				</li>
 			{/each}
-			{#each Array.from({ length: ARMY_CREATE_SPELL_FILLER - spellUnits.length > 0 ? ARMY_CREATE_SPELL_FILLER - spellUnits.length : 0 }) as filler}
-				<button class="object-card">
-					<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" class="add-icon">
-						<path
-							d="M15 0C11.0367 0.0480892 7.24926 1.64388 4.44657 4.44657C1.64388 7.24926 0.0480892 11.0367 0 15C0.0480892 18.9633 1.64388 22.7507 4.44657 25.5534C7.24926 28.3561 11.0367 29.9519 15 30C18.9633 29.9519 22.7507 28.3561 25.5534 25.5534C28.3561 22.7507 29.9519 18.9633 30 15C29.9519 11.0367 28.3561 7.24926 25.5534 4.44657C22.7507 1.64388 18.9633 0.0480892 15 0ZM23.5714 16.0714H16.0714V23.5714H13.9286V16.0714H6.42857V13.9286H13.9286V6.42857H16.0714V13.9286H23.5714V16.0714Z"
-							fill="#5C5C5C"
-						/>
-					</svg>
-				</button>
-			{/each}
+			{@render filler(ARMY_CREATE_SPELL_FILLER - spellUnits.length > 0 ? ARMY_CREATE_SPELL_FILLER - spellUnits.length : 0)}
 		</ul>
 		<h3>Select Spells</h3>
 		<ul class="picker-grid">
@@ -402,6 +384,12 @@
 		</div>
 	</div>
 </section>
+
+{#snippet filler(amount: number)}
+	{#each Array.from({ length: amount }) as filler}
+		<li class="object-card" />
+	{/each}
+{/snippet}
 
 <style>
 	/* TODO: move capacity styles to component */
