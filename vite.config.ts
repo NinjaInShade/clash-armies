463e343f7d0246948f7a57cc9e5c00a3492d4ea3
Leon Michalak <leonmichalak6@gmail.com>
2025-05-31 13:59:12 +0100

    Add dev network/https scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,37 @@
 /// <reference types="vitest" />
 
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import { coverageConfigDefaults } from 'vitest/config';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
 const changelogPath = fileURLToPath(new URL('CHANGELOG.md', import.meta.url));
 const changelog = readFileSync(changelogPath, 'utf8');
 
-export default defineConfig({
-	test: {
-		include: ['tests/app/**/*.ts'],
-		coverage: {
-			exclude: ['build/**', 'scripts/**', 'discord/**', 'svelte.config.js', ...coverageConfigDefaults.exclude],
+export default defineConfig((env) => {
+	const plugins: PluginOption[] = [sveltekit()];
+
+	// When running `vite dev`
+	if (env.command === 'serve') {
+		// Useful to test certain APIs like `navigator.share` which are only available on HTTPS.
+		// Running `npm run start:https` will automatically use this flag
+		if (process.env.DEV_HTTPS) {
+			plugins.push(basicSsl());
+		}
+	}
+
+	return {
+		test: {
+			include: ['tests/app/**/*.ts'],
+			coverage: {
+				exclude: ['build/**', 'scripts/**', 'discord/**', 'svelte.config.js', ...coverageConfigDefaults.exclude],
+			},
 		},
-	},
-	plugins: [sveltekit()],
-	define: {
-		__CHANGELOG__: JSON.stringify(changelog),
-	},
+		plugins,
+		define: {
+			__CHANGELOG__: JSON.stringify(changelog),
+		},
+	};
 });
