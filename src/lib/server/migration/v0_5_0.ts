ede5529b599eea5d96053e30818c2c5a2dc6c2a4
Leon Michalak <leonmichalak6@gmail.com>
2025-10-08 20:40:56 +0100

    Add new "Heroic Torch" GW equipment & new unit levels
    - Level 7 Lava Hound (and Ice Hound super troop)
    - Level 5 Ice Block Spell

--- src/lib/server/migration/v0_5_0.ts
+++ src/lib/server/migration/v0_5_0.ts
@@ -106,4 +106,53 @@ export default function migration(runStep: MigrationFn) {
 		INSERT INTO metrics (name, weight, minAgeHours) VALUES
 			('vote', 50, 1)
 	`);
+	runStep(53, async (db: MySQL) => {
+		// Insert data for new "Heroic Torch" equipment
+		const equipmentId = await db.insertOne('equipment', {
+			hero: 'Grand Warden',
+			name: 'Heroic Torch',
+			epic: 1,
+            clashId: 19,
+		});
+		// Insert levels
+		await db.insertMany('equipment_levels', [
+			{ equipmentId, level: 1, blacksmithLevel: 1 },
+			{ equipmentId, level: 2, blacksmithLevel: 1 },
+			{ equipmentId, level: 3, blacksmithLevel: 1 },
+			{ equipmentId, level: 4, blacksmithLevel: 1 },
+			{ equipmentId, level: 5, blacksmithLevel: 1 },
+			{ equipmentId, level: 6, blacksmithLevel: 1 },
+			{ equipmentId, level: 7, blacksmithLevel: 1 },
+			{ equipmentId, level: 8, blacksmithLevel: 1 },
+			{ equipmentId, level: 9, blacksmithLevel: 1 },
+			{ equipmentId, level: 10, blacksmithLevel: 1 },
+			{ equipmentId, level: 11, blacksmithLevel: 1 },
+			{ equipmentId, level: 12, blacksmithLevel: 1 },
+			{ equipmentId, level: 13, blacksmithLevel: 3 },
+			{ equipmentId, level: 14, blacksmithLevel: 3 },
+			{ equipmentId, level: 15, blacksmithLevel: 3 },
+			{ equipmentId, level: 16, blacksmithLevel: 5 },
+			{ equipmentId, level: 17, blacksmithLevel: 5 },
+			{ equipmentId, level: 18, blacksmithLevel: 5 },
+			{ equipmentId, level: 19, blacksmithLevel: 7 },
+			{ equipmentId, level: 20, blacksmithLevel: 7 },
+			{ equipmentId, level: 21, blacksmithLevel: 7 },
+			{ equipmentId, level: 22, blacksmithLevel: 8 },
+			{ equipmentId, level: 23, blacksmithLevel: 8 },
+			{ equipmentId, level: 24, blacksmithLevel: 8 },
+			{ equipmentId, level: 25, blacksmithLevel: 9 },
+			{ equipmentId, level: 26, blacksmithLevel: 9 },
+			{ equipmentId, level: 27, blacksmithLevel: 9 },
+		]);
+
+		// Insert data for new "Lava Hound" level
+		const lavaHound = await db.getRow<Unit>('units', { name: 'Lava Hound' });
+		const iceHound = await db.getRow<Unit>('units', { name: 'Ice Hound' });
+		await db.insertOne('unit_levels', { unitId: lavaHound.id, level: 7, barrackLevel: 6, laboratoryLevel: 14 });
+		await db.insertOne('unit_levels', { unitId: iceHound.id, level: 7, barrackLevel: 6, laboratoryLevel: 1 });
+
+		// Insert data for new "Ice Block" level
+		const iceBlock = await db.getRow<Unit>('units', { name: 'Ice Block' });
+        await db.insertOne('unit_levels', { unitId: iceBlock.id, level: 5, spellFactoryLevel: 7, laboratoryLevel: 15 });
+	});
 }
