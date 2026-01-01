0a5b93f91585009ebe2445a8672105d1078960ba
Leon Michalak <leonmichalak6@gmail.com>
2026-01-01 14:49:24 +0000

    Add new "Frost Flake" RC equipment

--- src/lib/server/migration/v0_5_0.ts
+++ src/lib/server/migration/v0_5_0.ts
@@ -344,4 +344,43 @@ export default function migration(runStep: MigrationFn) {
 		const wallWrecker = await db.getRow<Unit>('units', { name: 'Wall Wrecker' });
         await db.insertOne('unit_levels', { unitId: wallWrecker.id, level: 6, barrackLevel: 1, laboratoryLevel: 16 });
 	});
+	runStep(56, async (db) => {
+		// Insert data for new "Frost Flake" equipment
+		const equipmentId = await db.insertOne('equipment', {
+			hero: 'Royal Champion',
+			name: 'Frost Flake',
+			epic: 1,
+            clashId: 50,
+		});
+		// Insert levels
+		await db.insertMany('equipment_levels', [
+			{ equipmentId, level: 1, blacksmithLevel: 1 },
+			{ equipmentId, level: 2, blacksmithLevel: 1 },
+			{ equipmentId, level: 3, blacksmithLevel: 1 },
+			{ equipmentId, level: 4, blacksmithLevel: 1 },
+			{ equipmentId, level: 5, blacksmithLevel: 1 },
+			{ equipmentId, level: 6, blacksmithLevel: 1 },
+			{ equipmentId, level: 7, blacksmithLevel: 1 },
+			{ equipmentId, level: 8, blacksmithLevel: 1 },
+			{ equipmentId, level: 9, blacksmithLevel: 1 },
+			{ equipmentId, level: 10, blacksmithLevel: 1 },
+			{ equipmentId, level: 11, blacksmithLevel: 1 },
+			{ equipmentId, level: 12, blacksmithLevel: 1 },
+			{ equipmentId, level: 13, blacksmithLevel: 3 },
+			{ equipmentId, level: 14, blacksmithLevel: 3 },
+			{ equipmentId, level: 15, blacksmithLevel: 3 },
+			{ equipmentId, level: 16, blacksmithLevel: 5 },
+			{ equipmentId, level: 17, blacksmithLevel: 5 },
+			{ equipmentId, level: 18, blacksmithLevel: 5 },
+			{ equipmentId, level: 19, blacksmithLevel: 7 },
+			{ equipmentId, level: 20, blacksmithLevel: 7 },
+			{ equipmentId, level: 21, blacksmithLevel: 7 },
+			{ equipmentId, level: 22, blacksmithLevel: 8 },
+			{ equipmentId, level: 23, blacksmithLevel: 8 },
+			{ equipmentId, level: 24, blacksmithLevel: 8 },
+			{ equipmentId, level: 25, blacksmithLevel: 9 },
+			{ equipmentId, level: 26, blacksmithLevel: 9 },
+			{ equipmentId, level: 27, blacksmithLevel: 9 },
+		]);
+	});
 }
